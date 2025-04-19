de99d915a6b40c4320b9ba4a41450461719a88c3
contato.gprado@gmail.com <contato.gprado@gmail.com>
2025-04-19 09:19:51 -0300

    adiciona contagem de tentativas e exibição de resultado ao acertar o número

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
+const randomNumber = Math.round(Math.random() * 10)
+let xAttempts = 1
+
 function handleClick(event) {
   event.preventDefault()
 
   const inputNumber = document.querySelector('#inputNumber')
+  
+  const result = document.querySelector('#result')
 
-  console.log(inputNumber.value)
+  if(Number(inputNumber.value) == randomNumber) {
+    document.querySelector(".screen1").classList.add("hide")
+    document.querySelector(".screen2").classList.remove("hide")
+
+    document.querySelector(".screen2 h2")
+    .innerText = `Você acertou em ${xAttempts} tentativas!`
+    console.log(`Acertou em ${xAttempts} tentativas`)
+  }
+  
+  xAttempts++
 }
