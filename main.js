2e355622c34209e1d17fedd24959d632dc94630f
contato.gprado@gmail.com <contato.gprado@gmail.com>
2025-04-19 10:29:11 -0300

    refatora a estrutura do HTML e JavaScript e alterna as telas

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+
 const randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
-function handleClick(event) {
+// Função Callback
+function handleTryClick(event) {
   event.preventDefault()
 
   const inputNumber = document.querySelector('#inputNumber')
@@ -9,13 +13,28 @@ function handleClick(event) {
   const result = document.querySelector('#result')
 
   if(Number(inputNumber.value) == randomNumber) {
-    document.querySelector(".screen1").classList.add("hide")
-    document.querySelector(".screen2").classList.remove("hide")
+    screen1.classList.add("hide")
+    screen2.classList.remove("hide")
 
     document.querySelector(".screen2 h2")
     .innerText = `Você acertou em ${xAttempts} tentativas!`
     console.log(`Acertou em ${xAttempts} tentativas`)
   }
   
+  inputNumber.value = ""
   xAttempts++
 }
+
+// Eventos
+const btnTry = document.querySelector('#btnTry')
+const btnReset = document.querySelector('#btnReset')
+
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', function() {
+  screen1.classList.remove("hide")
+  screen2.classList.add("hide")
+  
+  xAttempts = 1
+  randomNumber = Math.round(Math.random() * 10)
+  console.log(randomNumber)
+})
